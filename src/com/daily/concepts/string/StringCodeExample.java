5c2d9326831a17ff7654b5996d1d34378bd6ca31
avinash dasar <avinash.dasar42@gmail.com>
2025-03-13 13:11:20 +0530

    split method tested

---
+++ src/com/daily/concepts/string/StringCodeExample.java
@@ -0,0 +1,13 @@
+package com.daily.concepts.string;
+
+public class StringCodeExample {
+	
+	public static void main(String[] args) {
+		String str = "Hello World";
+		//Split method
+		String[] strArray = str.split(" ");
+		System.out.println(strArray[0]);
+		System.out.println(strArray[1]);
+	}
+
+}
